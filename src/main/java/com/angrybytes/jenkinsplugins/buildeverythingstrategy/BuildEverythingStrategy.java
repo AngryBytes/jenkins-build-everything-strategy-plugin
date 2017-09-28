2e232de0e566a96cde344efc0b94b52aa99ebaa5
Stéphan Kochen <stephan@kochen.nl>
2017-09-28 20:16:37 +0200

    Annotate constructor with DataBoundConstructor

--- src/main/java/com/angrybytes/jenkinsplugins/buildeverythingstrategy/BuildEverythingStrategy.java
+++ src/main/java/com/angrybytes/jenkinsplugins/buildeverythingstrategy/BuildEverythingStrategy.java
@@ -1,12 +1,17 @@
 package com.angrybytes.jenkinsplugins.buildeverythingstrategy;
 
 import hudson.Extension;
+import org.kohsuke.stapler.DataBoundConstructor;
 import jenkins.branch.BranchBuildStrategy;
 import jenkins.branch.BranchBuildStrategyDescriptor;
 import jenkins.scm.api.SCMSource;
 import jenkins.scm.api.SCMHead;
 
 public class BuildEverythingStrategy extends BranchBuildStrategy {
+
+    @DataBoundConstructor
+    public BuildEverythingStrategy() {
+    }
 
     @Override
     public boolean isAutomaticBuild(SCMSource source, SCMHead head) {
